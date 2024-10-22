f6ce4d6758e0086a0a2bb7035043aa043fdde980
FelipeF-soares <felipe.fends@gmail.com>
2024-10-22 16:37:31 -0300

    1.Create Project configuration Scaffolding

---
+++ SmartParkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartParkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
